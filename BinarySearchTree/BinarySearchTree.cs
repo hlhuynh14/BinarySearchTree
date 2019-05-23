3790521f30a8ba9463e81b515eeab7d9f2560384
henry huynh <hlhuynh14@gmail.com>
2019-05-23 07:42:57 -0500

    init

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    { // member variables (HAS A)
+
+
+
+
+
+        // constructor
+        // member methods (CAN DO)
+        public void CreateBST()
+        {
+            Node start = null;
+            Tree binarySearchTree = new Tree();
+            int size = 32;
+            int[] newArray = new int[size];
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)  
+            {
+                newArray[i] = random.Next(10);
+
+            }
+            for (int i = 0; i < size; i++)
+            {
+                start = binarySearchTree.Insert(start, newArray[i]);
+            }
+
+            bool CheckTrue = binarySearchTree.Traverse(start, 7);
+            Console.WriteLine(CheckTrue);
+            Console.ReadLine();
+                
+        }
+        
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Tree
+    { // member variables (HAS A)
+      // constructor
+      // member methods (CAN DO)
+        public Node Insert(Node start, int value)
+        {
+
+            if (start == null)
+            {
+                start = new Node();
+                start.value = value;
+            }
+            else if (value < start.value)
+            {
+                start.leftNode = Insert(start.leftNode, value);
+            }
+
+            else
+            {
+                start.rightNode = Insert(start.rightNode, value);
+            }
+            return start;
+        }
+        public bool Traverse(Node start, int value)
+        {
+            if (start == null)
+            {
+                return false;
+            }
+            else if (start.value == value)
+            {
+                return true;
+            }
+            else if (start.value < value)
+            {
+                return Traverse(start.rightNode, value);
+            }
+            else if (start.value > value)
+            {
+                return Traverse(start.leftNode, value);
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+    }
+}
+
